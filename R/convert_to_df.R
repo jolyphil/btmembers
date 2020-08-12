aa78073f9e83ec5471e8cb23259535c51ce0bbb7
jolyphil <jolyphil@hu-berlin.de>
2020-08-12 21:41:17 +0200

    add if statement most_recent_name and add argument id_df to limit useless operations

--- R/convert_to_df.R
+++ R/convert_to_df.R
@@ -10,16 +10,15 @@ convert_to_df <- function(members_list, varlists){
 
 one_member_list_to_df <- function(one_member_list, varlists, pb) {
   pb$tick()$print()
-
+  id_df <- extract_one_value("ID", "id", one_member_list)
   name_vars_df <- one_member_list %>%
     extract_most_recent_name(varlists$name)
   bio_vars_df <- one_member_list[["BIOGRAFISCHE_ANGABEN"]] %>%
     extract_group_vars(varlists$bio)
   parlterm_vars_df <- one_member_list %>%
-    combine_parlterm_vars(varlists$parlterm)
-  one_member_df <- extract_one_value("ID", "id", one_member_list) %>%
-    dplyr::bind_cols(name_vars_df,
-              bio_vars_df) %>%
+    combine_parlterm_vars(varlists$parlterm, id_df)
+  one_member_df <- id_df %>%
+    dplyr::bind_cols(name_vars_df, bio_vars_df) %>%
     dplyr::left_join(parlterm_vars_df, by = "id")
   one_member_df
 }
@@ -27,15 +26,18 @@ one_member_list_to_df <- function(one_member_list, varlists, pb) {
 extract_most_recent_name <- function(one_member_list, varlist) {
   name_vars_df <- one_member_list[["NAMEN"]] %>%
     purrr::map_dfr(extract_group_vars, varlist) %>%
-    dplyr::mutate(historie_von = as.Date(historie_von, format = "%d.%m.%Y")) %>%
-    dplyr::filter(historie_von == max(historie_von))
+    dplyr::mutate(historie_von = as.Date(historie_von, format = "%d.%m.%Y"))
+  if (nrow(name_vars_df) > 1) {
+    name_vars_df <- name_vars_df %>%
+      dplyr::filter(historie_von == max(historie_von))
+  }
   name_vars_df
 }
 
-combine_parlterm_vars <- function(one_member_list, varlist) {
+combine_parlterm_vars <- function(one_member_list, varlist, id_df) {
   parlterm_vars_df <- one_member_list[["WAHLPERIODEN"]] %>%
     purrr::map_dfr(extract_group_vars, varlist)
-  id <- extract_one_value("ID", "id", one_member_list)[["id"]] %>%
+  id <- id_df[["id"]] %>%
     rep(times = nrow(parlterm_vars_df))
   parlterm_vars_df <- parlterm_vars_df %>%
     dplyr::bind_cols(id = id)
