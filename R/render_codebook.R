8fa1d39874d8bdab0431122f359e57a95095ba21
jolyphil <jolyphil@hu-berlin.de>
2020-09-01 17:07:09 +0200

    publish pdf instead of md

--- R/render_codebook.R
+++ R/render_codebook.R
@@ -1,5 +1,5 @@
 render_codebook <- function(){
   rmarkdown::render("vignettes/codebook.Rmd",
-                    output_format = "github_document",
+                    output_format = "pdf_document",
                     output_dir = "./codebook")
 }
