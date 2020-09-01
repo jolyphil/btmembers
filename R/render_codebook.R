60bf8d86f58c13e924533a6e8ebe227895cd8bb8
jolyphil <jolyphil@hu-berlin.de>
2020-09-01 16:35:18 +0200

    render markdown for GitHub

--- R/render_codebook.R
+++ R/render_codebook.R
@@ -1,3 +1,5 @@
 render_codebook <- function(){
-  rmarkdown::render("vignettes/codebook.Rmd", output_dir = "./codebook")
+  rmarkdown::render("vignettes/codebook.Rmd",
+                    output_format = "github_document",
+                    output_dir = "./codebook")
 }
