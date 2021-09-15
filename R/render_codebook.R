bbc1972f5aceb721ade67e45f59dbf07ec5b8d4c
Philippe Joly <philippe.joly@posteo.net>
2021-09-15 14:04:49 +0200

    add information codebook

--- R/render_codebook.R
+++ R/render_codebook.R
@@ -14,19 +14,38 @@ save_codebook_md <- function(members_list) {
               "",
               paste0("Version: ", attr(members_list, "version")),
               "",
+              "The btmembers R package restructures the open data provided by the Bundestag. See:",
+              "",
+              paste0("> Bundestag (",
+                     format(attr(members_list, "version"), format = "%Y"),
+                     "), _Stammdaten aller Abgeordneten seit 1949 im XML-Format_, ",
+                     "version: ",
+                     attr(members_list, "version"),
+                     ", https://www.bundestag.de/services/opendata."),
+              "",
+              "By default, the function `import_members()` returns a list containing four data frames: `namen` (names), `bio` (biographical information), `wp` (parliamentary terms), and `inst` (institutions). These four data frames are presented below.",
+              "",
               "---",
               "")
   var_blocks <- purrr::map2(names(members_list),
                             members_list,
                             codebook_var_block) %>%
     unlist()
-  codebook <- c(header, var_blocks)
+  footer <- "If `import_members()` is called with the argument `condensed_df = TRUE`, the function will return a condensed data frame. Each row corresponds to a member-term. Most of the information contained in the original data is preserved except _only the most recent name of the member is retained_ and _institutions are removed_. A new column named `fraktion` is added to the data. `fraktion` is a recoded variable and refers to the faction the member spent most time in during a given parliamentary term."
+  codebook <- c(header, var_blocks, footer)
   writeLines(codebook, "codebook/codebook.md")
 }
 
 codebook_var_block <- function(dfname, df) {
+
+  n_row <- nrow(df)
+  n_members <- df$id %>%
+    unique() %>%
+    length()
+  n_col <- ncol(df)
+
   c(paste0("## Data frame `", dfname, "`"),
-    paste0("`", dfname, "` contains the following columns:" ),
+    paste0("`", dfname, "` contains ", n_row, " observations from ",  n_members, " members of the Bundestag. It includes ", n_col, " variables:"),
     "",
     codebook_var_list(df),
     "---",
--- codebook/codebook.md
+++ codebook/codebook.md
@@ -4,10 +4,16 @@
 
 Version: 2021-03-12
 
+The btmembers R package restructures the open data provided by the Bundestag. See:
+
+> Bundestag (2021), _Stammdaten aller Abgeordneten seit 1949 im XML-Format_, version: 2021-03-12, https://www.bundestag.de/services/opendata.
+
+By default, the function `import_members()` returns a list containing four data frames: `namen` (names), `bio` (biographical information), `wp` (parliamentary terms), and `inst` (institutions). These four data frames are presented below.
+
 ---
 
 ## Data frame `namen`
-`namen` contains the following columns:
+`namen` contains 4363 observations from 4089 members of the Bundestag. It includes 10 variables:
 
 ### `id`
 
@@ -72,7 +78,7 @@ Version: 2021-03-12
 ---
 
 ## Data frame `bio`
-`bio` contains the following columns:
+`bio` contains 4089 observations from 4089 members of the Bundestag. It includes 12 variables:
 
 ### `id`
 
@@ -149,7 +155,7 @@ Version: 2021-03-12
 ---
 
 ## Data frame `wp`
-`wp` contains the following columns:
+`wp` contains 11627 observations from 4089 members of the Bundestag. It includes 9 variables:
 
 ### `id`
 
@@ -208,7 +214,7 @@ Version: 2021-03-12
 ---
 
 ## Data frame `inst`
-`inst` contains the following columns:
+`inst` contains 15858 observations from 4089 members of the Bundestag. It includes 9 variables:
 
 ### `id`
 
@@ -266,3 +272,4 @@ Version: 2021-03-12
 
 ---
 
+If `import_members()` is called with the argument `condensed_df = TRUE`, the function will return a condensed data frame. Each row corresponds to a member-term. Most of the information contained in the original data is preserved except _only the most recent name of the member is retained_ and _institutions are removed_. A new column named `fraktion` is added to the data. `fraktion` is a recoded variable and refers to the faction the member spent most time in during a given parliamentary term.
