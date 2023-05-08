03f9dbcefdfc3b967251f1ff4972dc71452ba93a
Philippe Joly <philippe.joly@posteo.net>
2023-05-08 20:23:13 +0200

    Change argument to data_source and integrate check for DTD file

--- R/import_members.R
+++ R/import_members.R
@@ -5,27 +5,30 @@
 #' four data frames nested into a list (retaining all the information of the
 #' original XML file) or (b) to a single, condensed data frame.
 #'
-#' @param condensed_df logical; if `TRUE` join the four data frames contained
-#'     in the default list into a single, condensed data frame. Warning: Some
-#'     data is lost (see Value).
-#' @param force_from_bt logical; if `TRUE` force the function to import the data
-#'     directly from the Bundestag website. By default, the function compares
-#'     the versions of the data on the Bundestag website and the pre-processed
-#'     data stored in the package repository on GitHub. If the version on GitHub
-#'     is the same as the one on the Bundestag website, the function downloads
-#'     the data from GitHub. If the version on the Bundestag website is more
-#'     recent, the user will be presented with a menu of three choices:
-#'     (1) Download the new, non-tested version from the Bundestag website;
-#'     (2) Download the older, pre-processed data from Github; or (3) Cancel.
-#'     `force_from_bt = TRUE` never imports the data from GitHub.
-#' @return If **`condensed_df = FALSE`** a list containing four data frames:
-#'     `namen` (names), `bio` (biographical information), `wp`
-#'     (parliamentary terms), and `inst` (institutions).
+#' @param condensed_df logical; if `TRUE`, the function joins the four data
+#'     frames contained in the default list into a single, condensed data frame.
+#'     Warning: Some data is lost (see Value).
 #'
-#'    A codebook with a full list of variables is available
+#' @param data_source character: `"auto"`, `"Bundestag"`, or `"GitHub"`. With
+#'     the value `"auto"`, the function compares the versions of the data on the
+#'     Bundestag website and the pre-processed data stored in the package
+#'     repository on GitHub. If the version on GitHub is the same as the one on
+#'     the Bundestag website, the function downloads the data from GitHub. If
+#'     the version on the Bundestag website is more recent, the function checks
+#'     if there were changes in the documentation of the XML file. If not,
+#'     the function imports and processes the raw data from the Bundestag.
+#'     With the value `"Bundestag"`, the function tests if the documentation of
+#'     the XML file has been changed. If not, the function imports and processes
+#'     the raw data from the Bundestag.
+#'     With the value `"GitHub"`, the function imports the preprocessed data
+#'     from the package repository on GitHub.
+#'
+#' @return If `condensed_df = FALSE`, a list containing four data frames:
+#'     `namen` (names), `bio` (biographical information), `wp`
+#'     (parliamentary terms), and `inst` (institutions). A **codebook** with a full list of variables is available
 #'    [here](https://github.com/jolyphil/btmembers/blob/main/codebook/codebook.pdf).
 #'
-#'    If **`condensed_df = TRUE`** a condensed data frame. Each row corresponds
+#'    If `condensed_df = TRUE`, a condensed data frame. Each row corresponds
 #'    to a member-term. Most of the information contained in the original data
 #'    is preserved _except_ only the most recent name of the member is retained
 #'    and institutions are removed. A new column named `fraktion` is added to
@@ -44,31 +47,43 @@
 
 
 import_members <- function(condensed_df = FALSE,
-                           force_from_bt = FALSE) {
+                           data_source = "auto") {
 
-  link_info <- extract_link_info()
+  if (!(data_source %in% c("auto", "Bundestag", "GitHub"))) {
+    stop("The argument data_source has to be either \"auto\", \"Bundestag\", or \"GitHub\". See ?import_members().")
+  }
 
-  if (force_from_bt == FALSE) {
+  if (data_source %in% c("auto", "Bundestag")) {
+    link_info <- extract_link_info()
+  }
+
+  if (data_source == "auto") {
     version_github <- extract_github_version()
-    decision <- import_new(link_info$version_bt, version_github)
-    if (decision == 1) {
-      force_from_bt <- TRUE
-    }
-    if (decision == 3) {
-      stop("Cancelling data import.")
+    if (link_info$version_bt > version_github) {
+      data_source <- "Bundestag"
+    } else {
+      data_source <- "GitHub"
     }
   }
 
-  if (force_from_bt) {
+  if (data_source == "Bundestag") {
     paste0("Downloading primary data (version: ",
            link_info$version_bt,
            ") from the Bundestag website") %>%
       message()
+    temp_zip_file <- tempfile()
+    download.file(link_info$href, temp_zip_file)
+    if (!test_dtd(temp_zip_file)) {
+      stop("The Bundestag seems to have changed the format of the XML file used by btmembers. You can download the previous version of the data using the argument data_source = \"GitHub\".  Consider opening an issue here: https://github.com/jolyphil/btmembers/issues")
+    }
     data_version <- link_info$version_bt
-    list_raw <- import_list_raw(link_info$href)
+    list_raw <- import_list_raw(temp_zip_file)
     list_clean <- restructure_list(list_raw)
     attr(list_clean, "version") <- data_version
-  } else {
+  } else { # data_source == "GitHub"
+    if (!exists("version_github")) {
+      version_github <- extract_github_version()
+    }
     paste0("Downloading pre-processed data (version: ",
            version_github,
            ") from GitHub") %>%
@@ -130,6 +145,12 @@ extract_link_info <- function() {
   link_info
 }
 
+extract_github_version <- function() {
+  version_url <- paste0(github_storage_url(), "data_version.rds")
+  data_version <- url(version_url) %>%
+    readRDS()
+  data_version
+}
 
 github_storage_url <- function(branch = "main") {
   storage_url <- paste0("https://github.com/jolyphil/btmembers/raw/",
@@ -138,15 +159,6 @@ github_storage_url <- function(branch = "main") {
   storage_url
 }
 
-
-extract_github_version <- function() {
-  version_url <- paste0(github_storage_url(), "data_version.rds")
-  data_version <- url(version_url) %>%
-    readRDS()
-  data_version
-}
-
-
 extract_github_list <- function() {
   list_url <- paste0(github_storage_url(), "members_list.rds")
   members_list <- url(list_url) %>%
@@ -154,24 +166,25 @@ extract_github_list <- function() {
   members_list
 }
 
-import_new <- function(version_bt, version_github){
-  if (version_bt > version_github) {
-    decision <- utils::menu(
-      title = "A new version of the data is available on the Bundestag website, but it has not been tested yet. Would you like to import this new version anyway?",
-      choices =c("Yes, try importing the new version of the data from the Bundestag website.",
-                 "No, import the preprocessed data from GitHub.",
-                 "Cancel."))
-  } else {
-    decision <- 2
-  }
-    decision
+download_zip_file <- function(href){
+  temp_zip_file <- tempfile()
+  download.file(href, temp_zip_file)
+  temp_zip_file # Returns location of temporary ZIP file
 }
 
-import_list_raw <- function(href){
-  temp <- tempfile()
-  download.file(href, temp)
+test_dtd <- function(temp_zip_file) {
+  con <- temp_zip_file %>%
+    unz("MDB_STAMMDATEN.DTD")
+  test_passed <- con %>%
+    readLines() %>%
+    identical(ref_dtd)
+  close(con)
+  test_passed
+}
+
+import_list_raw <- function(temp_zip_file){
   message("Converting XML file to list...")
-  list_raw <- unz(temp, "MDB_STAMMDATEN.XML") %>% # Unzip XML data
+  list_raw <- unz(temp_zip_file, "MDB_STAMMDATEN.XML") %>% # Unzip XML data
     xml2::read_xml() %>%
     xml2::as_list() %>%
     `[[`("DOCUMENT") %>% # Select first nested list
